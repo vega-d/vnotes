15e6e69db0d9cba51c68eb21ec047f9a66cde835
vega-d <vegad0100110@gmail.com>
2022-01-23 13:48:06 +0100

    introducing a bootstrap.sh script for the flatpak autobuild

---
+++ bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "bootstrapping all required files for the flatpak."
+required_files=("src/main.py" "src/mainmenu.py" "src/conf.py" "src/dropdown.py"\
+ "src/editor.py" "src/fileframework.py" "src/settingsmenu.py" "src/run.sh"\
+ "io.github.vegad.vnotes.desktop" "io.github.vegad.vnotes.appdata.xml" "icons/icon.svg"\
+ "icons/icon_48.png" "icons/icon_128.png" "LICENSE.txt")
+gitlink="https://raw.githubusercontent.com/vega-d/vnotes/master/"
+mkdir "src"
+for file in "${required_files[@]}"
+do
+  rm "$file"
+  current_link="${gitlink}""${file}"
+  curl "$current_link" > "$file"
+done
+echo "finished the download of all required files for the flatpak."
+
